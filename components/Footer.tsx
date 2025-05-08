77ff89fd29308d926fab319e8a707f6b866ce15e
nopianpdlh <novianfadhilah03@gmail.com>
2025-05-08 23:06:42 +0700

    initial project

---
+++ components/Footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div></div>
+    </footer>
+  );
+};
+
+type FooterColumnProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const FooterColumn = ({ title, children }: FooterColumnProps) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
